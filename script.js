ce7b8efb736b6dfd550aa3e9fe2d09bf549d27ca
bittoMazza <robertomazza99@gmail.com>
2022-06-28 13:29:21 +0200

    created team array

---
+++ script.js
@@ -0,0 +1,33 @@
+const team = [
+    {
+        name : "Wayne Barnett",
+        role : "Founder & CEO",
+        imageUrl : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name : "Angela Caroll",
+        role : "Chief Editor",
+        imageUrl : "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name : "Walter Gordon",
+        role : "Office Manager",
+        imageUrl : "walter-gordon-office-manager.jpg"
+    },
+    {
+        name : "Angela Lopez",
+        role : "Social Media Manager",
+        imageUrl : "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name : "Scott Estrada",
+        role : "Developer",
+        imageUrl : "scott-estrada-developer.jpg"
+    },
+    {
+        name : "Barbara Ramos",
+        role : "Graphic Designer",
+        imageUrl : "barbara-ramos-graphic-designer.jpg"
+    }
+
+]
